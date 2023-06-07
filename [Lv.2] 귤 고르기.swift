49d7b8f51325ad5c0d3ed3c241ce2b486c86e5d6
LeeJaeheee <74818845+LeeJaeheee@users.noreply.github.com>
2023-06-07 22:32:47 +0900

    [Programmers_138476] 귤 고르기

--- [Lv.2] 귤 고르기.swift
+++ [Lv.2] 귤 고르기.swift
@@ -21,3 +21,26 @@ func solution(_ k:Int, _ tangerine:[Int]) -> Int {
     }
     return result
 }
+
+// 수정한 코드
+import Foundation
+
+func solution(_ k:Int, _ tangerine:[Int]) -> Int {
+    if tangerine.count == k {
+        return Set(tangerine).count
+    }
+    var dict: [Int: Int] = [:]
+    for t in tangerine {
+        dict[t, default: 0] += 1
+    }
+    var result = 0
+    var count = 0
+    for d in dict.values.sorted(by: > ) {
+        count += d
+        result += 1
+        if count >= k {
+            break
+        }
+    }
+    return result
+}
